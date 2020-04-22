4f505c7486d45f17b64c8fb556ad37fb65470cab
Alexander Jack <jackx022@umn.edu>
2020-04-22 12:54:42 -0500

    add fab button onclicklistener to mainactivity

--- app/src/main/java/com/example/lifecounter2/MainActivity.java
+++ app/src/main/java/com/example/lifecounter2/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.lifecounter2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = (Toolbar) findViewById(R.id.toolBarLayoutID);
         viewPager = (ViewPager) findViewById(R.id.viewPagerLayoutID);
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -29,5 +32,12 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         setSupportActionBar(toolbar);
         tabLayout.setupWithViewPager(viewPager);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "hello world", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
